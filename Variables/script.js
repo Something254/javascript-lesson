a1bc8c36fe7ebf9821780335524fc0bdb4ce8d26
Someting254 <onyiaorah.zimife@rosevilleschool.org>
2024-07-30 20:31:36 +0100

    my first js script

---
+++ Variables/script.js
@@ -0,0 +1,27 @@
+/*
+let variable1 = 3;
+console.log(variable1);
+
+let kamsi = "coding"+"="+ 10;
+console.log(kamsi);
+
+let calculation = 2+2;
+console.log(calculation);
+
+console.log(calculation+2);
+
+let result = calculation+2;
+console.log(result);
+
+variable1 =5*2;
+console.log(variable1);
+
+variable1 =variable1 + 1;
+console.log(variable1);
+*/
+
+let a = null;
+console.log(a);
+
+let b;
+console.log(b);
---
+++ arrays/arrays.js
@@ -0,0 +1,117 @@
+// const names = ['john', 'david', 'mary']
+
+// console.log(names)
+
+// let cars = [];
+// cars[0] = 'volvo';
+// cars[1] = 'benz';
+
+// cars[0] = 'toyota';
+
+// cars.push('lexus')
+
+// console.log(cars)
+
+
+
+// // const person = {
+// // firstname: "Zimife",
+// // lastname: "Onyiaorah",
+// // age: 14,
+// // eyeColor:"brown"
+// // }
+
+// // console.log(person)
+// //
+
+
+// let classNames = [];
+// classNames[0] = 'David';
+// classNames[1] = 'Zimife';
+// classNames[2] = 'Kaima';
+
+// classNames[0] = 'Kamsi';
+
+
+// classNames.push('Amanda')
+
+// console.log(classNames)
+
+
+
+// const myLaptop = {
+//     manufacturer: "HP",
+//     model: "EliteBook 840 G1",
+//     ram: "8.00GB",
+//     hardDrive:"464GB"
+//     }
+    
+//     console.log(myLaptop)
+
+
+
+
+//     const person = {};
+//     person.firstname = "John";
+//     person.lastname = "Doe";
+//     console.log(person);
+
+//     const cities = new Object();
+//     cities.anambra = "Awka";
+//     cities.abia = "Umuahia";
+//     cities.niger = "Minna";
+//     console.log(cities);
+
+//     console.log(cities.anambra);
+
+//     const goat = new Object();
+//     goat.diet ="grass";
+//     goat.habitat ="Terrestial";
+//     goat.size = "Large";
+   
+//     console.log(goat.diet);
+//     console.log(goat.habitat);
+//     console.log(goat.size);
+
+//     console.log(goat["diet"]);
+//     console.log(goat["habitat"]);
+//     console.log(goat["size"]);
+
+//     delete goat.diet;
+//     delete goat["habitat"];
+//     console.log(goat)
+
+
+    const zimife = {};
+    zimife.firstname = "Zimife";
+    zimife.lastname = "Onyiaorah";
+    zimife.age = 14;
+    zimife.gender = "Female";
+    zimife.complexion = "Fair";
+    console.log(zimife);
+
+    console.log(zimife.firstname);
+    console.log(zimife.lastname);
+
+    console.log(zimife["age"]);
+    console.log(zimife["gender"]);
+    console.log(zimife["complexion"]);
+
+    delete zimife.age;
+    delete zimife["lastname"];
+    console.log(zimife)
+
+
+
+
+    
+    const cow = new Object();
+    cow.diet ="grass";
+    cow.habitat ="Terrestial";
+    cow.size = "Large";
+    cow.stomachType = "Ruminant";
+    cow.bodyMorphology = "Mammals";
+    cow.anatomy = "Vertebrates";
+    cow.gestationPeriod = "273.75 days";
+    
+    console.log(cow);
